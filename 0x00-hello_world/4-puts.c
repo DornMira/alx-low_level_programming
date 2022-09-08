73967465720ecdd9cb1cfd28cc7862480933913e
DornMira <dorinekhainza@gmail.com>
2022-09-08 03:37:57 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Use the function puts
+ *
+ *
+ * Your program should end with the value 0
+ *
+**/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
