57071850cc7382386b92fdbc558f4e821a55458c
DornMira <dorinekhainza@gmail.com>
2022-09-30 18:16:48 -0700

    print a number, followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that prints the number of arguements you passed into it
+ * @argc: An argument counter
+ * @argv: An argument values
+ * Return: Always 0 (Success)
+ **/
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
